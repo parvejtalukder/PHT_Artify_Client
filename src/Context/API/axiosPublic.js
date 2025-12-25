e57bce16977f1578429925fec686d4eeb9c5f60b
Parvej Husen Talukder <mail@parvejhusentalukder.com>
2025-12-25 11:15:45 +0600

    Update axiosPublic.js

--- src/Context/API/axiosPublic.js
+++ src/Context/API/axiosPublic.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosPublic = axios.create({
-  baseURL: 'https://artify.parvejhusentalukder.com',
+  baseURL: 'https://app.parvejhusentalukder.com',
 });
 
 export default axiosPublic;
